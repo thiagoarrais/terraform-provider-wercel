da13fb5d33317426ce069731fb6f73f2e7b0b3f3
Thiago Arrais <thiago.arrais@gmail.com>
2021-05-31 21:21:37 -0300

    chore: make it debuggable

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/thiagoarrais/terraform-provider-wercel/wercel"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return wercel.Provider()
-		},
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/thiagoarrais/wercel",
+			&plugin.ServeOpts{
+				ProviderFunc: wercel.Provider,
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: wercel.Provider,
+		})
+	}
 }
